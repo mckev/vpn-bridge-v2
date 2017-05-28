52f2b90fc6905c105740d5929470480e9e982a1d
Kevin Haritmonds <haritmonds@yahoo.com>
2017-05-28 19:51:25 +0530

    Compile on Raspbian Jessie

--- compile.sh
+++ compile.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-# Pre-requisites:
-#    yum install -y gcc-c++
-
 machine=`uname -m`
-if [ "$machine" == "x86_64" ]; then
-    echo "x86_64"
-    rm -f Packet.o
-    rm -f vclient.o
-    rm -f vclient
-    g++ -std=c++11 -Wall -m64 -O3 -o Packet.o   -c Packet.cpp
-    g++ -std=c++11 -Wall -m64 -O3 -o vclient.o  -c vclient.cpp
-    g++ -std=c++11 -Wall -m64 -O3 -o vclient    Packet.o vclient.o
-
-elif [ "$machine" == "armv6l" ]; then
-    echo "armv6l"
-
+if [ "$machine" = "x86_64" ]; then
+    # Pre-requisites:
+    #    yum install -y gcc-c++
+    echo "Processor: x86_64"
+    CXXFLAGS="-m64"
+elif [ "$machine" = "armv6l" ]; then
+    echo "Processor: armv6l"
+    CXXFLAGS=""
 else
     echo "Unsupported processor $machine"
+    exit 1
 fi
+
+echo "CXXFLAGS: $CXXFLAGS"
+echo "Compiling..."
+rm -f Packet.o
+rm -f vclient.o
+rm -f vclient
+g++ -std=c++11 -Wall $CXXFLAGS -O3 -o Packet.o   -c Packet.cpp
+g++ -std=c++11 -Wall $CXXFLAGS -O3 -o vclient.o  -c vclient.cpp
+g++ -std=c++11 -Wall $CXXFLAGS -O3 -o vclient    Packet.o vclient.o
+echo "Done."
