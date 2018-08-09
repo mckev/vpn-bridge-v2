f00101b37c2f493d931e3a4528d5645d9f39aa6e
Kevin Haritmonds <haritmonds@yahoo.com>
2018-08-09 23:14:03 +0530

    Change the UDP payload into "Hello, world!"

--- vpn-bridge-test/Packet-test-ip.cc
+++ vpn-bridge-test/Packet-test-ip.cc
@@ -3,17 +3,16 @@
 
 
 static uint8_t packet[20] = {
-	0x45, 0x00, 0x00, 0x28, 0x95, 0x88, 0x40, 0x00, 0x35, 0x11, 0x74, 0x98, 0xdf, 0xe5, 0xce, 0x87,
+	0x45, 0x00, 0x00, 0x29, 0x92, 0x23, 0x40, 0x00, 0x35, 0x11, 0x5b, 0xe9, 0x6a, 0xc9, 0x5f, 0xb7,
 	0xc6, 0x9a, 0xc6, 0x9c
 };
 
 
 TEST(PacketTestIp, ConvertIpAddressFromUint32IntoStr) {
-	uint32_t ip_addr;
-	ip_addr = 0x87cee5df;
-	EXPECT_EQ(std::string("223.229.206.135"), Ip::ip_addr_to_str(ip_addr));
-	ip_addr = 0x9cc69ac6;
-	EXPECT_EQ(std::string("198.154.198.156"), Ip::ip_addr_to_str(ip_addr));
+	uint32_t ip_addr1 = 0xb75fc96a;
+	EXPECT_EQ(std::string("106.201.95.183"), Ip::ip_addr_to_str(ip_addr1));
+	uint32_t ip_addr2 = 0x9cc69ac6;
+	EXPECT_EQ(std::string("198.154.198.156"), Ip::ip_addr_to_str(ip_addr2));
 }
 
 
@@ -21,11 +20,11 @@ TEST(PacketTestIp, ParseIpPacketCorrectly) {
 	Ip* ip = (Ip*)packet;
 	EXPECT_EQ(4, ip->version);
 	EXPECT_EQ(20, ip->header_len());
-	EXPECT_EQ(40, ip->total_len());
-	EXPECT_EQ(38280, ntohs(ip->id));
+	EXPECT_EQ(41, ip->total_len());
+	EXPECT_EQ(37411, ntohs(ip->id));
 	EXPECT_EQ(53, ip->ttl);
 	EXPECT_EQ(Ip::IPPROTO_UDP, ip->protocol);
-	EXPECT_EQ(std::string("223.229.206.135"), Ip::ip_addr_to_str(ip->saddr));
+	EXPECT_EQ(std::string("106.201.95.183"), Ip::ip_addr_to_str(ip->saddr));
 	EXPECT_EQ(std::string("198.154.198.156"), Ip::ip_addr_to_str(ip->daddr));
 }
 
